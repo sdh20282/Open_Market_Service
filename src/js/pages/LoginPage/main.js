5343835cbf3e7183c5c633366b9396e6a5909ec3
donghyeon.seo <sdh20282@gmail.com>
2022-11-24 20:25:49 +0900

    update Open Market

--- src/js/pages/LoginPage/main.js
+++ src/js/pages/LoginPage/main.js
@@ -5,12 +5,14 @@ export default () => {
     
     const purchaseLogin = document.createElement('button');
     purchaseLogin.style.width = '275px';
+    purchaseLogin.style.height = '80px';
     purchaseLogin.textContent = '구매회원 로그인';
     purchaseLogin.style.border = 'border: 1px solid #C4C4C4';
     purchaseLogin.style.borderRadius = '10px';
 
     const registerLogin = document.createElement('button');
     registerLogin.style.width = '275px';
+    registerLogin.style.height = '80px';
     registerLogin.textContent = '판매회원 로그인';
     registerLogin.style.border = 'border: 1px solid #C4C4C4';
     registerLogin.style.borderRadius = '10px';
